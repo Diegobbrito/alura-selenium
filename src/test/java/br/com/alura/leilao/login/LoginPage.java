832513a4ee341f14f5e65cfb7d6b4d45def01f5a
Diego Brito <dibbrito@gmail.com>
2021-05-24 15:25:33 -0300

    refactor using pageObject pattern

---
+++ src/test/java/br/com/alura/leilao/login/LoginPage.java
@@ -0,0 +1,53 @@
+package br.com.alura.leilao.login;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LoginPage {
+    private WebDriver browser;
+
+    public LoginPage() {
+        System.setProperty("webdriver.chrome.driver", "drivers/chromedriver.exe");
+        this.browser = new ChromeDriver();
+        browser.navigate().to("http://localhost:8080/login");
+    }
+
+    public void fechar() {
+        this.browser.quit();
+    }
+
+    public void preencheFormularioDeLogin(String username, String password) {
+        browser.findElement(By.id("username")).sendKeys(username);
+        browser.findElement(By.id("password")).sendKeys(password);
+    }
+
+    public void submeteFormulario() {
+        browser.findElement(By.id("login-form")).submit();
+    }
+
+    public boolean isPaginaDeLogin() {
+        return browser.getCurrentUrl().equals("http://localhost:8080/login");
+    }
+
+    public String getNomeUsuarioLogado() {
+        try {
+            return browser.findElement(By.id("usuario-logado")).getText();
+        } catch (NoSuchElementException e) {
+            return null;
+        }
+    }
+
+    public void navegaParaPaginadDeLances() {
+        browser.navigate().to("http://localhost:8080/leiloes/2");
+    }
+
+    public boolean contemTexto(String s) {
+        return browser.getPageSource().contains(s);
+    }
+
+    public boolean isPaginaDeLoginComDadosInvalidos() {
+        return browser.getCurrentUrl().equals("http://localhost:8080/login?error");
+    }
+}
